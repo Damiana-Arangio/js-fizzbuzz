d2855de4aac95484b11afd8669d1c32a7797859f
Damiana-Arangio <arangio.damiana@gmail.com>
2025-08-28 20:47:52 +0200

    Implementazione completata:
    - Generazione numeri con ciclo for
    
    - Gestione output: Fizz, Buzz, FizzBuzz o numero

--- js/script.js
+++ js/script.js
@@ -4,3 +4,28 @@
     ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
     Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 */
+
+
+for (let i = 1 ; i <= 100 ; i++) {
+
+    // Multipli di 3 e di 5
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+    }
+
+    // Multipli di 3
+    else if (i % 3 == 0) {
+        console.log("Fizz"); 
+    }
+
+    // Multipli di 5
+    else if (i % 5 == 0) {
+        console.log("Buzz");
+    }
+
+    // Non multipli di 3 o 5
+    else {
+        console.log(i);
+    }
+
+}
